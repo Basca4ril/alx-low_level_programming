067ad53ff64a0e5f1da8b7b620c8ac66d876024d
Basca4ril <basianchls@gmail.com>
2023-08-10 01:33:23 -0700

    updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+//Function prints a statement then returns 0
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
