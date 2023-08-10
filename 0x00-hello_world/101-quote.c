5ae9f2283a67a1640d54f002e2e9b1d28b71193e
Basca4ril <basianchls@gmail.com>
2023-08-10 03:06:42 -0700

    Added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,9 @@
 int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char newline = '\n';
 	write(2, msg, strlen(msg));
+	write(STDERR_FILENO, &newline, 1);
 
 	return (1);
 }
