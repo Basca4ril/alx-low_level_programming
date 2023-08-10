36703f573b186da61331c509c934eec99a0bfec8
Basca4ril <basianchls@gmail.com>
2023-08-10 02:50:40 -0700

    Added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 1 (success)
+ */
+
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, msg, strlen(msg));
+	return 1;
+}
