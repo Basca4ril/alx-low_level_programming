dd2d99a44b293c5071f302077592a688474fcef0
Basca4ril <basianchls@gmail.com>
2023-08-15 14:24:54 -0700

    Addded 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int fb1 = 1;
+	int fb2 = 2;
+	int fnxt;
+
+	printf("%d, %d", fb1, fb2);
+
+	int i;
+
+	for (i = 3; i <= 50; i++)
+	{
+		fnxt = fb1 +fb2;
+		if (i < 50)
+		{
+			printf("%d, " fnxt);
+		}
+		else
+		{
+			printf("%d", fnxt);
+		}
+		fb1 = fb2;
+		fb2 = fnxt;
+	}
+	printf("\n");
+	return (0);
+}
