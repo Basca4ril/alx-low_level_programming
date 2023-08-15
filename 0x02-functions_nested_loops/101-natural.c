a62c91db2d248a2922e3f2e5e3cc0c03b8bc373f
Basca4ril <basianchls@gmail.com>
2023-08-15 14:10:01 -0700

    Added 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 3; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("The sum is: %d\n", sum);
+	return (0);
+}
