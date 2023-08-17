041b9611bce4b7aa71026cd26ede58de42b5891d
Basca4ril <basianchls@gmail.com>
2023-08-17 06:57:35 -0700

    Added 8-print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - prints square
+ *
+ * @size: number of hashes
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		if (size <= 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');	
+	}
+	_putchar('\n');
+}
