bb7b9cf3b43875e85c70b01c2c943b7386bf383c
Basca4ril <basianchls@gmail.com>
2023-08-17 05:16:51 -0700

    Added 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - prints line
+ * @n: number of underscores
+ */
+void print_line(int n)
+{
+	int i;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
