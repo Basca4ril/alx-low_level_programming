85e9bb63cc0e6531a484c215651480db86d5fe55
Basca4ril <basianchls@gmail.com>
2023-08-17 06:30:34 -0700

    Added 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - prints diagonal
+ *
+ * @n: number of backslash
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+		if (n <= 0)
+		{
+			_putchar('\n');
+			break;
+		}
+	}
+	_putchar('\n');
+}
