2f5f80625d4ac6e3a8aa017eaad4086aa886f8b6
Basca4ril <basianchls@gmail.com>
2023-08-28 11:23:28 -0700

    Added 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main"
+#include <stdio.h>
+
+/**
+ * _strspn - gets length 
+ * @s: string to search
+ * @accept - pointer for characters
+ * Return: length
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int len = 0;
+
+	while (*s != '\0' && strchr(accept, *s) != NULL)
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
