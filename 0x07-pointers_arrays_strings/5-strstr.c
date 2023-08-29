921fab254c6ab466a05b14f8de7a80365dd51801
Basca4ril <basianchls@gmail.com>
2023-08-28 17:54:09 -0700

    added

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - find substring
+ * @haystack: string
+ * @needle: substring
+ * Return: substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	if (needle == '\0')
+		return (haystack);
+
+	char *hay;
+	char *ned;
+
+	while (*haystack != '\0')
+	{
+		hay = haystack;
+		ned = needle;
+
+		while (*ned != '\0' && *hay == *ned)
+		{
+			hay++;
+			ned++;
+		}
+
+		if (*ned == '\0')
+			return (haystack);
+
+		haystack++;
+	}
+	return (NULL);
+}
