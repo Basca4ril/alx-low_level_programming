8f10f1fac2a2da9ad3d9ec6813226127dea064b1
Basca4ril <basianchls@gmail.com>
2023-08-30 01:33:11 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - find factorial
+ * @n: parameter for fatorial
+ * Return: factorial
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n + factorial(n - 1));
+}
