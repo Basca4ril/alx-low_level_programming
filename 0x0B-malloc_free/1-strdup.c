96eae20ed96c107b3c0656751684382214c49c32
Basca4ril <basianchls@gmail.com>
2023-09-05 00:53:18 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - duplicates string
+ * @str: Original string
+ * Return: pointer to duplicate string
+ */
+
+char *_strdup(char *str)
+{
+	size_t len = strlen(str);
+	char *dup = (char *)malloc(sizeof(char) * (len + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	if (dup == NULL)
+		return (NULL);
+
+	strcpy(dup, str);
+
+	return (dup);
+}
