39a41a6d864663378bc29821e814da975d76c0bb
Basca4ril <basianchls@gmail.com>
2023-09-07 03:02:37 -0700

    Updated 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,7 +9,7 @@
  */
 int *array_range(int min, int max)
 {
-	int i, *ptr;
+	int i, *ptr, diff;
 
 	if (min > max)
 		return (NULL);
--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char *concat;
 
-	concat =_calloc(98, sizeof(char));
+	concat = array_range(0, 10);
 	printf("%s\n", concat);
 	free(concat);
 	return (0);
