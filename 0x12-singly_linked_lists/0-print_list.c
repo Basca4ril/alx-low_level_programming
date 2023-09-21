568a0232be8044cbfa40ccf6fc634b8f249299b8
Basca4ril <basianchls@gmail.com>
2023-09-21 04:27:44 -0700

    Added main.h README.md and 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_list - prints elements of list_t list
+ * @h: pointer to list_t
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	
+	list_t *ptr = NULL;
+	ptr = h;
+
+	while (ptr != NULL)
+	{
+		count++;
+		if (ptr->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%u] %s",h->len, h->str);
+
+		h = h->next;
+	}
+	return (count);
+
+}
