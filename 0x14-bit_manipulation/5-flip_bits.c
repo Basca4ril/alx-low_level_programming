88561de600aaf9328d6a3fad0be72de572cc90a4
Basca4ril <basianchls@gmail.com>
2023-09-28 16:24:51 -0700

    Added 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts flips
+ * @n: number 1
+ * @m: number 2
+ * Return: number of flips
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m;
+	unsigned int cnt = 0;
+
+	while (xor)
+	{
+		cnt += xor & 1;
+		xor >> 1;
+	}
+
+	return (cnt);
+}
