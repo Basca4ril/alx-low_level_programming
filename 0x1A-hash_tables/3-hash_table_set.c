441af7171d204d691c3a72071dc9a85821ba33d0
Basca4ril <basianchls@gmail.com>
2023-12-02 02:55:51 +0200

    bug fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,7 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (newnode->key == NULL)
 		return (0);
 
-	newnode->key - strdup(key);
+	newnode->key = strdup(key);
 	if (newnode->key == NULL)
 	{
 		free(newnode);
